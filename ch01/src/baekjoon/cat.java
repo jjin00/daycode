943740b579daf1ed536baef0931a2e5ed7e577a7
jin <jin@DESKTOP-VVUEU2V>
2022-06-04 18:14:53 +0900

    고양이

---
+++ ch01/src/baekjoon/cat.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+
+public class cat {
+
+	public static void main(String[] args) {
+	
+	System.out.println("\\    /\\");
+	System.out.println(" )  ( ')");
+	System.out.println("(  /  )");
+	System.out.printf(" \\(__)|");
+		
+	}
+
+}
